e590b55d02529b8e597825358fc4360f0c5d12d6
Łukasz Zabuski <riraseth@gmail.com>
2020-05-23 02:07:40 +0200

    added FormInput component to bmi calculator component

--- src/components/BmiCalculator.js
+++ src/components/BmiCalculator.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FormInput from './FormInput';
 
 const BmiCalculator = () => {
+  const [heightUnit, setHeightUnit] = useState('cm');
+  const [weightUnit, setWeightUnit] = useState('kg');
+  const [unit, setUnit] = useState('Metric');
+
+  const onChangeInput = (e) => {};
+  const onSelectTag = (e) => {
+    setUnit(e.target.value);
+    if (e.target.value === 'Metric') {
+      setHeightUnit('cm');
+      setWeightUnit('kg');
+    } else {
+      setHeightUnit('ft');
+      setWeightUnit('lb');
+    }
+  };
+
   return (
     <React.Fragment>
       <div className="bmi-inputs">
@@ -10,7 +27,8 @@ const BmiCalculator = () => {
             <div className="unit">
               <select
                 name="unit"
-                value=""
+                value={unit}
+                onChange={onSelectTag}
                 className="form-control form-control-sm"
               >
                 <option value="Metric">Metric</option>
@@ -18,7 +36,29 @@ const BmiCalculator = () => {
               </select>
             </div>
           </div>
-          <input type="text" />
+          <FormInput
+            type="text"
+            name="heightCount"
+            title={`Height (${heightUnit})`}
+            value=""
+            onChange={onChangeInput}
+          />
+          {unit === 'Imperial' ? (
+            <FormInput
+              type="text"
+              name="inchesCount"
+              title={`(in)`}
+              value=""
+              onChange={onChangeInput}
+            />
+          ) : null}
+          <FormInput
+            type="text"
+            name="weightCount"
+            title={`Weight (${weightUnit})`}
+            value=""
+            onChange={onChangeInput}
+          />
         </div>
         <button className="button" type="submit">
           Reset
